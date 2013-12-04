d52d54370c7e582c7eb956fc63fe092793503fa6
eastseven <eastseven.dongq@gmail.com>
2013-12-04 17:38:01 +0800

    导出存储过程

---
+++ export_procedure.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+var_dbname=game_db
+var_dbuser=mysql
+var_dbpass=123456
+var_dbhost=10.10.10.99
+
+var_tmpdir=tmp
+
+PWD=`pwd`
+MYSQL=`which mysql`
+MYSQLDUMP=`which mysqldump`
+
+if [ -z "$MYSQLDUMP" ]; then
+	MYSQLDUMP=/usr/local/mysql/bin/mysqldump
+fi
+
+if [ -z "$MYSQL" ]; then
+	MYSQL=/usr/local/mysql/bin/mysql
+fi
+
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > $PWD/$var_tmpdir/${var_dbname}_sp.sql
+
+var_dbname=game_count
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > $PWD/$var_tmpdir/${var_dbname}_sp.sql
+
+var_dbname=game_user
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > $PWD/$var_tmpdir/${var_dbname}_sp.sql
