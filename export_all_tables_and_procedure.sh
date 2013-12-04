be21a62179fdd56d918d85a66b12e905c4d5353d
eastseven <eastseven.dongq@gmail.com>
2013-12-04 16:12:11 +0800

    导出表、存储过程、系统数据

--- export_all_tables_and_procedure.sh
+++ export_all_tables_and_procedure.sh
@@ -7,19 +7,58 @@ var_dbhost=10.10.10.99
 
 var_systable=`cat system_table.txt`
 
+PWD=`pwd` 
 MYSQL=`which mysql`
 MYSQLDUMP=`which mysqldump`
 
-echo `date "+%Y-%m-%d %H:%M:%S"`': 导出游戏服表结构、存储过程'
-$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > ${var_dbname}_exp.sql
+if [ -z "$MYSQLDUMP" ]; then
+	MYSQLDUMP=/usr/local/mysql/bin/mysqldump
+fi
 
-echo `date "+%Y-%m-%d %H:%M:%S"`': 导出游戏服系统数据'
-$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -t ${var_dbname} ${var_systable} > ${var_dbname}_sys_data.sql
+if [ -z "$MYSQL" ]; then
+	MYSQL=/usr/local/mysql/bin/mysql
+fi
+
+var_all_table=game_db_all_table.sql
+var_sysdata_file=game_db_sysdata.sql
+var_tmpdir=tmp
+rm -rf $var_tmpdir
+mkdir $var_tmpdir
+
+echo `date "+%Y-%m-%d %H:%M:%S"`': 导出游戏服表结构、存储过程' >> merge.log
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > $PWD/$var_tmpdir/${var_all_table}
+echo `date "+%Y-%m-%d %H:%M:%S"`': 导出游戏服系统数据' >> merge.log
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -t ${var_dbname} ${var_systable} > $PWD/$var_tmpdir/${var_sysdata_file}
+
+var_dbname=game_count
+var_all_table=game_count_all_table.sql
+var_sysdata_file=game_count_sysdata.sql
+var_systable=`cat system_table_gamecount.txt`
+echo `date "+%Y-%m-%d %H:%M:%S"`': 导出游戏服统计表结构、存储过程' >> merge.log
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > $PWD/$var_tmpdir/${var_all_table}
+echo `date "+%Y-%m-%d %H:%M:%S"`': 导出游戏服统计系统数据' >> merge.log
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -t ${var_dbname} ${var_systable} > $PWD/$var_tmpdir/${var_sysdata_file}
+
 
 var_dbname=game_user
 var_systable=`cat system_table_gameuser.txt`
-echo `date "+%Y-%m-%d %H:%M:%S"`': 导出登录服表结构、存储过程'
-$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > ${var_dbname}_exp.sql
+echo `date "+%Y-%m-%d %H:%M:%S"`': 导出登录服表结构、存储过程' >> merge.log
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -d -R ${var_dbname} > $PWD/$var_tmpdir/${var_dbname}_all_table.sql
 
-echo `date "+%Y-%m-%d %H:%M:%S"`': 导出登录服系统数据'
-$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -t ${var_dbname} ${var_systable} > ${var_dbname}_sys_data.sql
+echo `date "+%Y-%m-%d %H:%M:%S"`': 导出登录服系统数据' >> merge.log
+$MYSQLDUMP -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} -n -t ${var_dbname} ${var_systable} > $PWD/$var_tmpdir/${var_dbname}_sysdata.sql
+
+#var_dbname=game_db_002
+#var_dbhost=10.10.10.99
+#echo `date "+%Y-%m-%d %H:%M:%S"`': 导入游戏服表结构、存储过程' >> merge.log
+#$MYSQL ${var_dbname} -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} < $PWD/$var_tmpdir/${var_dbname}_exp.sql
+#echo `date "+%Y-%m-%d %H:%M:%S"`': 导入游戏服系统数据' >> merge.log
+#$MYSQL ${var_dbname} -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} < $PWD/$var_tmpdir/${var_dbname}_sys_data.sql
+
+#var_dbname=game_user
+#echo `date "+%Y-%m-%d %H:%M:%S"`': 导入登录服表结构、存储过程'
+#$MYSQL ${var_dbname} -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} < $PWD/$var_tmpdir/${var_dbname}_exp.sql
+#echo `date "+%Y-%m-%d %H:%M:%S"`': 导入登录服系统数据'
+#$MYSQL ${var_dbname} -h${var_dbhost} -u${var_dbuser} -p${var_dbpass} < $PWD/$var_tmpdir/${var_dbname}_sys_data.sql
+
+echo `date "+%Y-%m-%d %H:%M:%S"`': 完成'
--- merge_data.sh
+++ merge_data.sh
@@ -1,58 +1,61 @@
 #!/bin/sh
 
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''合并服务器数据'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''合并服务器数据' >> merge.log
 
 # ==================== 变量定义 ====================
 MYSQL_DUMP=`which mysqldump`
 MYSQL=`which mysql`
 
 var_current_path=`pwd`
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''当前路径：'$var_current_path
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''当前路径：'$var_current_path >> merge.log
 
 var_hefu_sp=merge_data_procedure.sql
 
 # 原数据库
-var_source_ip=10.10.10.22
-var_source_db=game_db_228
+var_source_ip=10.10.10.99
+var_source_db=game_db_001
 var_source_user=mysql
 var_source_psw=123456
-var_source_userdata=$var_source_db'_userdata_bak.sql'
+var_source_userdata=source_db_userdata_bak.sql
+var_usertable=`cat user_table.txt`
+var_source_sockport=17701
+var_source_httpport=17801
 
 # 目标数据库
-var_target_ip=10.10.10.22
-var_target_db=game_db_229
+var_target_ip=10.10.10.99
+var_target_db=game_db_002
 var_target_user=mysql
 var_target_psw=123456
+var_target_sockport=27701
+var_target_httpport=27801
 
 # 登录服数据库
-var_login_ip=10.10.10.22
-var_login_db=game_user_246
+var_login_ip=10.10.10.99
+var_login_db=game_user_000
 var_login_user=mysql
 var_login_psw=123456
 
 # ==================== 华丽的分割线 ====================
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前，备份数据库'
-#$MYSQL_DUMP -h$var_source_ip -u$var_source_user -p$var_source_psw \
-#--skip-comments --compact --no-create-db --databases $var_source_db > $var_source_db'_source_bak.sql'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前，备份数据库' >> merge.log
+$MYSQL_DUMP -h$var_source_ip -u$var_source_user -p$var_source_psw --skip-comments --compact --no-create-db --databases $var_source_db > ${var_source_db}'_source_bak.sql'
 
-#MYSQL_DUMP -h$var_target_ip -u$var_target_user -p$var_target_psw \
-#--skip-comments --compact --no-create-db --databases $var_target_db > $var_target_db'_target_bak.sql'
+$MYSQL_DUMP -h$var_target_ip -u$var_target_user -p$var_target_psw --skip-comments --compact --no-create-db --databases $var_target_db > ${var_target_db}'_target_bak.sql'
 
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前source库UID数据'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前source库UID数据' >> merge.log
 $MYSQL $var_source_db -h$var_source_ip -u$var_source_user -p$var_source_psw <<EOF
 select count(uid) from role;
 EOF
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前target库UID数据'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前target库UID数据' >> merge.log
 $MYSQL $var_target_db -h$var_target_ip -u$var_target_user -p$var_target_psw <<EOF
 select count(uid) from role;
 EOF
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前target库中重复source库中的UID数据'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第零步 合服前target库中重复source库中的UID数据' >> merge.log
 $MYSQL -h$var_target_ip -u$var_target_user -p$var_target_psw <<EOF
 select r8.uid from $var_source_db.role r8 where exists (select 1 from $var_target_db.role r9 where r8.uid = r9.uid);
 EOF
 
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第一步 合服迁移数据导出(在命令行执行)'
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''----数据清理：调用每个合服数据库的数据清理存储过程:call sp_hefu_clear() ----'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第一步 合服迁移数据导出(在命令行执行)' >> merge.log
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''----数据清理：调用每个合服数据库的数据清理存储过程:call sp_hefu_clear() ----' >> merge.log
 $MYSQL $var_source_db -h$var_source_ip -u$var_source_user -p$var_source_psw <<EOF
 call sp_hefu_clear(1);
 EOF
@@ -60,39 +63,28 @@ $MYSQL $var_target_db -h$var_target_ip -u$var_target_user -p$var_target_psw <<EO
 call sp_hefu_clear(1);
 EOF
 
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表导出(主要用于合服)开始 ----'
-$MYSQL_DUMP -h$var_source_ip -u$var_source_user -p$var_source_psw \
---databases $var_source_db --skip-comments --no-create-db --no-create-info \
---tables builds cd_queue email_receive email_send frdgroup friend hero_equip \
-hero_foster hero_skill hero_train lvuprank message_family message_system message_user \
-message_world mymessage role role_achieve role_backeg role_bonus role_castle role_data \
-role_equip role_ext role_form role_goddess role_hero role_hero_bak role_hero_great role_hero_refrish \
-role_luck role_log role_log_bmoney role_log_charm role_log_day role_log_war role_native role_present role_prop_use \
-role_props role_renown role_scence role_sports role_sports_enemy role_task role_task_target role_explore leave leave_reply \
-message_leave_word role_activities role_log_goods role_fb_invite hero_log > $var_source_userdata
-#role_hunt_data role_hunt_item \
-#role_hunt_log role_hunt_luck \
-#> $var_source_userdata
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表导出(主要用于合服)，导出文件：'$var_source_userdata'----'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表导出(主要用于合服)开始 ----' >> merge.log
+$MYSQL_DUMP -h$var_source_ip -u$var_source_user -p$var_source_psw --databases $var_source_db --skip-comments --no-create-db --no-create-info --tables ${var_usertable} > $var_source_userdata
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表导出(主要用于合服)，导出文件：'$var_source_userdata'----' >> merge.log
 
 
 # ==================== 华丽的分割线 ====================
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第二步 将数据导入目标数据库'
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表，导入开始 ----'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第二步 将数据导入目标数据库' >> merge.log
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表，导入开始 ----' >> merge.log
 $MYSQL $var_target_db -h$var_target_ip -u$var_target_user -p$var_target_psw < $var_source_userdata
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表，导入结束 ----'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''----用户数据表，导入结束 ----' >> merge.log
 
 
 # ==================== 华丽的分割线 ====================
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第三步 执行存储过程进行数据处理 call sp_hefu();'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第三步 执行存储过程进行数据处理 call sp_hefu();' >> merge.log
 $MYSQL $var_target_db -h$var_target_ip -u$var_target_user -p$var_target_psw <<EOF
 call sp_hefu();
 EOF
 
 
 # ==================== 华丽的分割线 ====================
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第四步 手工修改登录服务器中被合服服务器的主机ip地址（host_group）'
-var_update_sql="update host_group set ip='${var_target_ip}', lip='${var_target_ip}', game_db='${var_target_db}' where ip='$var_source_ip' and lip='$var_source_ip' and game_db='$var_source_db';"
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第四步 手工修改登录服务器中被合服服务器的主机ip地址（host_group）' >> merge.log
+var_update_sql="update host_group set ip='${var_target_ip}', lip='${var_target_ip}', game_db='${var_target_db}', sock_port='${var_target_sockport}', http_port='${var_target_httpport}' where ip='$var_source_ip' and lip='$var_source_ip' and game_db='$var_source_db';"
 $MYSQL $var_login_db -h$var_login_ip -u$var_login_user -p$var_login_psw <<EOF
 $var_update_sql ;
 commit;
@@ -100,10 +92,10 @@ commit;
 EOF
 
 # ==================== 华丽的分割线 ====================
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''第五步 合服后target库UID数据'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''第五步 合服后target库UID数据' >> merge.log
 $MYSQL $var_target_db -h$var_target_ip -u$var_target_user -p$var_target_psw <<EOF
 select count(uid) from role;
 EOF
 
 # ==================== 华丽的分割线 ====================
-echo `date "+%Y-%m-%d %H:%M:%S"`': ''服务器重启'
+echo `date "+%Y-%m-%d %H:%M:%S"`': ''服务器重启' >> merge.log
