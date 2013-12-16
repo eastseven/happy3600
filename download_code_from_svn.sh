83ed55a38a70a426ddeb94a95e6c8e7435ec2dfb
eastseven <eastseven.dongq@gmail.com>
2013-12-16 17:49:47 +0800

    modify

--- download_code_from_svn.sh
+++ download_code_from_svn.sh
@@ -9,9 +9,9 @@ rm -rf $project
 mkdir $project
 cd $project
 
-svn checkout --usename $svn_user --pasword $svn_password $svn_url/conf conf
-svn checkout --usename $svn_user --pasword $svn_password $svn_url/language language
-svn checkout --usename $svn_user --pasword $svn_password $svn_url/src src
-svn checkout --usename $svn_user --pasword $svn_password $svn_url/build.properties build.properties
-svn checkout --usename $svn_user --pasword $svn_password $svn_url/build.xml build.xml
-svn checkout --usename $svn_user --pasword $svn_password $svn_url/startserver.sh startserver
+svn checkout --username $svn_user --pasword $svn_password $svn_url/conf conf
+svn checkout --username $svn_user --pasword $svn_password $svn_url/language language
+svn checkout --username $svn_user --pasword $svn_password $svn_url/src src
+svn checkout --username $svn_user --pasword $svn_password $svn_url/build.properties build.properties
+svn checkout --username $svn_user --pasword $svn_password $svn_url/build.xml build.xml
+svn checkout --username $svn_user --pasword $svn_password $svn_url/startserver.sh startserver
