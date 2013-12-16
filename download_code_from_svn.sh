3095e0b10ff0691b045107f5fdc15f198b8c9a2a
eastseven <eastseven.dongq@gmail.com>
2013-12-16 18:00:58 +0800

    done

--- download_code_from_svn.sh
+++ download_code_from_svn.sh
@@ -9,9 +9,9 @@ rm -rf $project
 mkdir $project
 cd $project
 
-svn checkout --username $svn_user --pasword $svn_password $svn_url/conf conf
-svn checkout --username $svn_user --pasword $svn_password $svn_url/language language
-svn checkout --username $svn_user --pasword $svn_password $svn_url/src src
-svn checkout --username $svn_user --pasword $svn_password $svn_url/build.properties build.properties
-svn checkout --username $svn_user --pasword $svn_password $svn_url/build.xml build.xml
-svn checkout --username $svn_user --pasword $svn_password $svn_url/startserver.sh startserver
+svn checkout --username $svn_user --password $svn_password $svn_url/conf conf
+svn checkout --username $svn_user --password $svn_password $svn_url/language language
+svn checkout --username $svn_user --password $svn_password $svn_url/src src
+svn cat --username $svn_user --password $svn_password $svn_url/build.properties > build.properties
+svn cat --username $svn_user --password $svn_password $svn_url/build.xml > build.xml
+svn cat --username $svn_user --password $svn_password $svn_url/startserver.sh > startserver
